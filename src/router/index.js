bd2f77c4c9ce09f0d7c5adbcef392ac2fb533932
Pokola <13811708195@163.com>
2018-01-24 00:42:06 +0800

    change welcome

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Wellcome from '@/components/Wellcome'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
@@ -14,8 +14,8 @@ export default new Router({
     },
     {
       path: '/page1',
-      name: 'wellcome',
-      component: Wellcome,
+      name: 'welcome',
+      component: Welcome,
     },
   ]
 })
